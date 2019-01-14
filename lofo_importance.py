ff68209183111479eea164fa107b493c93c8bc66
Stephane Collot <stephane.collot@gmail.com>
2019-01-14 13:50:22 +0100

    Standardize with scikit-learn scorer and cross-validator
    - CV
    - Scorer
    - Random seeds fixed in notebooks

--- lofo_importance.py
+++ lofo_importance.py
@@ -1,57 +1,38 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_validate
 from tqdm import tqdm_notebook
 
 
 class LOFOImportance:
 
-    def __init__(self, model, metric, df, features, target,
-                 needs_proba=True, greater_is_better=True, num_folds=4):
+    def __init__(self, model, df, features, target,
+                 scoring, cv=4):
         self.model = model
-        self.metric = metric
         self.df = df
         self.features = features
         self.target = target
-        self.needs_proba = needs_proba
-        self.sign = 1 if greater_is_better else -1
-        self.num_folds = num_folds
-
-    def _calculate_performance(self, X_train, y_train, X_val, y_val):
-        self.model.fit(X_train, y_train)
-        if self.needs_proba:
-            y_pred = self.model.predict_proba(X_val)[:, 1]
-        else:
-            y_pred = self.model.predict(X_val)
-
-        return self.metric(y_val, y_pred)
+        self.scoring = scoring
+        self.cv = cv
 
     def _get_cv_score(self, X, y):
-        importance = np.zeros(self.num_folds)
-        kf = KFold(n_splits=self.num_folds, shuffle=True, random_state=0)
-
-        for fold, (train_index, val_index) in enumerate(kf.split(X, y)):
-            X_train, y_train = X.iloc[train_index], y.iloc[train_index]
-            X_val, y_val = X.iloc[val_index], y.iloc[val_index]
-
-            importance[fold] = self._calculate_performance(X_train, y_train, X_val, y_val)
-
-        return importance
+        cv_results = cross_validate(self.model, X, y, cv=self.cv, scoring=self.scoring)
+        return cv_results['test_score']
 
     def get_importance(self):
-        importances = np.zeros((len(self.features), self.num_folds))
+        lofo_cv_scores = []
 
-        base_importance = self._get_cv_score(self.df[self.features], self.df[self.target])
+        base_cv_score = self._get_cv_score(self.df[self.features], self.df[self.target])
 
         for i, f in tqdm_notebook(enumerate(self.features)):
             feature_list = [feature for feature in self.features if feature != f]
-            importances[i, :] = base_importance - self._get_cv_score(self.df[feature_list], self.df[self.target])
+            lofo_cv_scores.append(self._get_cv_score(self.df[feature_list], self.df[self.target]))
 
-        importances *= self.sign
+        lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score for lofo_cv_score in lofo_cv_scores])
 
         importance_df = pd.DataFrame()
         importance_df["feature"] = self.features
-        importance_df["importance_mean"] = importances.mean(axis=1)
-        importance_df["importance_std"] = importances.std(axis=1)
+        importance_df["importance_mean"] = lofo_cv_scores_normalized.mean(axis=1)
+        importance_df["importance_std"] = lofo_cv_scores_normalized.std(axis=1)
 
         return importance_df.sort_values("importance_mean", ascending=False)
