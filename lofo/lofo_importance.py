85bc3dbaf20a4b609ef68149e377b949a49000fe
rafah-ek <rafah.elkhatib@gmail.com>
2019-01-21 19:06:07 +0000

    Added multiprocessing to the LOFO class (#6)
    * Added multiprocessing to the LOFO class

--- lofo/lofo_importance.py
+++ lofo/lofo_importance.py
@@ -2,33 +2,64 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_validate
 from tqdm import tqdm_notebook
+import multiprocessing
+import warnings
 
 
 class LOFOImportance:
 
     def __init__(self, model, df, features, target,
-                 scoring, cv=4):
+                 scoring, cv=4, n_jobs=None):
         self.model = model
         self.df = df
         self.features = features
         self.target = target
         self.scoring = scoring
         self.cv = cv
+        self.n_jobs = n_jobs
+        if self.n_jobs is not None and self.n_jobs > 1:
+            warning_str = "Warning: If your model is multithreaded, please initialise the number \
+                of jobs of LOFO to be equal to 1, otherwise you may experience issues."
+            warnings.warn(warning_str)
 
     def _get_cv_score(self, X, y):
         cv_results = cross_validate(self.model, X, y, cv=self.cv, scoring=self.scoring)
         return cv_results['test_score']
 
-    def get_importance(self):
-        lofo_cv_scores = []
+    def _get_cv_score_parallel(self, feature, feature_list, result_queue, base=False):
+        cv_results = cross_validate(self.model, self.df[feature_list], self.df[self.target],
+                                    cv=self.cv, scoring=self.scoring)
+        if not base:
+            result_queue.put((feature, cv_results['test_score']))
+        return cv_results['test_score']
 
+    def get_importance(self):
         base_cv_score = self._get_cv_score(self.df[self.features], self.df[self.target])
 
-        for i, f in tqdm_notebook(enumerate(self.features)):
-            feature_list = [feature for feature in self.features if feature != f]
-            lofo_cv_scores.append(self._get_cv_score(self.df[feature_list], self.df[self.target]))
+        if self.n_jobs is not None and self.n_jobs > 1:
+            pool = multiprocessing.Pool(self.n_jobs)
+            manager = multiprocessing.Manager()
+            result_queue = manager.Queue()
 
-        lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score for lofo_cv_score in lofo_cv_scores])
+            base_cv_score = self._get_cv_score_parallel('all', self.features, result_queue, True)
+            for f in self.features:
+                feature_list = [feature for feature in self.features if feature != f]
+                pool.apply_async(self._get_cv_score_parallel, (f, feature_list, result_queue))
+
+            pool.close()
+            pool.join()
+
+            lofo_cv_scores = [result_queue.get() for _ in range(len(self.features))]
+            lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score
+                                                  for f, lofo_cv_score in lofo_cv_scores])
+            self.features = [score[0] for score in lofo_cv_scores]
+        else:
+            lofo_cv_scores = []
+            for i, f in tqdm_notebook(enumerate(self.features)):
+                feature_list = [feature for feature in self.features if feature != f]
+                lofo_cv_scores.append(self._get_cv_score(self.df[feature_list], self.df[self.target]))
+
+            lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score for lofo_cv_score in lofo_cv_scores])
 
         importance_df = pd.DataFrame()
         importance_df["feature"] = self.features
--- tests/test_lofo_importance.py
+++ tests/test_lofo_importance.py
@@ -1,6 +1,8 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from lofo.lofo_importance import LOFOImportance
 from data.test_data import generate_test_data
+from lightgbm import LGBMRegressor
+from sklearn.model_selection import KFold
 
 
 def test_lofo_importance():
@@ -8,9 +10,25 @@ def test_lofo_importance():
 
     features = ["A", "B", "C", "D"]
 
-    rf = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0)
+    lgbm = LGBMRegressor(random_state=0, n_jobs=4)
 
-    lofo = LOFOImportance(rf, df, features, 'binary_target', cv=4, scoring='roc_auc')
+    lofo = LOFOImportance(lgbm, df, features, 'binary_target', cv=4, scoring='roc_auc')
+
+    importance_df = lofo.get_importance()
+
+    assert len(features) == importance_df.shape[0], "Missing importance value for some features!"
+    assert importance_df["feature"].values[0] == "B", "Most important feature is different than B!"
+
+
+def test_multithreading():
+    df = generate_test_data(100000)
+
+    features = ["A", "B", "C", "D"]
+
+    lr = LogisticRegression(solver='liblinear')
+    cv = KFold(n_splits=4, shuffle=True, random_state=0)
+
+    lofo = LOFOImportance(lr, df, features, 'binary_target', cv=cv, scoring='roc_auc', n_jobs=3)
 
     importance_df = lofo.get_importance()
 
