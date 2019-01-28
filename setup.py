53c347762e99e7b22f1ac06d9c3d9ef850436e90
Stephane Collot <stephane.collot@gmail.com>
2019-01-28 11:36:26 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lofo-importance',
+    version='0.0.2',
+    packages=['lofo'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "sklearn",
+    ],
+)
