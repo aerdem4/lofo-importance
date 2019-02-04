eb1ad77ef90d1e4952c3c08679cdf72b1820d5e6
Ahmet Erdem <ahmeterd4@gmail.com>
2019-02-04 19:42:29 +0100

    Make setup.py use requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='lofo-importance',
     version='0.0.2',
     packages=['lofo'],
-    install_requires=[
-        "numpy",
-        "pandas",
-        "sklearn",
-    ],
+    install_requires=requirements
 )
