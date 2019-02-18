8f3a10b7deb1d2337916719129ae5a2d9dcfc762
Ahmet Erdem <ahmeterd4@gmail.com>
2019-02-18 20:57:49 +0100

    Make LOFO pip installable

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='lofo-importance',
-    version='0.0.2',
-    packages=['lofo'],
-    install_requires=requirements
+    version='0.1.1',
+    author="Ahmet Erdem",
+    author_email="ahmeterd4@gmail.com",
+    description="Leave One Feature Out Importance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
