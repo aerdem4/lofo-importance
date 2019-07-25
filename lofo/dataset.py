80fe8469353cd2d11723aee6be3f410eaf752bd8
aerdem4 <ahmeterd4@gmail.com>
2019-07-25 09:49:26 +0200

    Add feature group support

---
+++ lofo/dataset.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.sparse as ss
+
+
+class Dataset:
+
+    def __init__(self, df, target, features, feature_groups=None):
+        self.df = df.copy()
+        self.features = list(features)
+        self.feature_groups = feature_groups if feature_groups else dict()
+
+        self.num_rows = df.shape[0]
+        self.y = df[target].values
+
+        for feature_name, feature_matrix in self.feature_groups.items():
+            if not (isinstance(feature_matrix, np.ndarray) or isinstance(feature_matrix, ss.csr.csr_matrix)):
+                raise Exception("Data type {dtype} is not a valid type!".format(dtype=type(feature_matrix)))
+
+            if feature_matrix.shape[0] != self.num_rows:
+                raise Exception("Expected {expected} rows but got {n} rows!".format(expected=self.num_rows,
+                                                                                    n=feature_matrix.shape[0]))
+
+    def getX(self, feature_to_remove, fit_params):
+        feature_list = [feature for feature in self.features if feature != feature_to_remove]
+        concat_list = [self.df[feature_list].values]
+
+        for feature_name, feature_matrix in self.feature_groups.items():
+            if feature_name != feature_to_remove:
+                concat_list.append(feature_matrix)
+
+        fit_params = fit_params.copy()
+        if "categorical_feature" in fit_params:
+            cat_features = [f for f in fit_params["categorical_feature"] if f != feature_to_remove]
+            fit_params["categorical_feature"] = np.where(cat_features)[0].tolist()
+
+        has_sparse = False
+        for feature_name, feature_matrix in self.feature_groups.items():
+            if feature_name != feature_to_remove and isinstance(feature_matrix, ss.csr.csr_matrix):
+                has_sparse = True
+
+        concat = np.hstack
+        if has_sparse:
+            concat = ss.hstack
+
+        return concat(concat_list), fit_params
--- lofo/lofo_importance.py
+++ lofo/lofo_importance.py
@@ -9,72 +9,66 @@ from lofo.infer_defaults import infer_model
 
 class LOFOImportance:
 
-    def __init__(self, df, features, target,
-                 scoring, model=None, cv=4, n_jobs=None):
+    def __init__(self, dataset, scoring, model=None, fit_params=None, cv=4, n_jobs=None):
 
-        df = df.copy()
-        self.fit_params = {}
+        self.fit_params = fit_params if fit_params else dict()
         if model is None:
-            model, df, categoricals = infer_model(df, features, target, n_jobs)
+            model, dataset.df, categoricals, dataset.y = infer_model(dataset.df, dataset.features, dataset.y, n_jobs)
             self.fit_params["categorical_feature"] = categoricals
             n_jobs = 1
 
         self.model = model
-        self.df = df
-        self.features = features
-        self.target = target
+        self.dataset = dataset
         self.scoring = scoring
         self.cv = cv
         self.n_jobs = n_jobs
         if self.n_jobs is not None and self.n_jobs > 1:
-            warning_str = "Warning: If your model is multithreaded, please initialise the number \
-                of jobs of LOFO to be equal to 1, otherwise you may experience issues."
+            warning_str = ("Warning: If your model is multithreaded, please initialise the number"
+                           "of jobs of LOFO to be equal to 1, otherwise you may experience performance issues.")
             warnings.warn(warning_str)
 
-    def _get_cv_score(self, X, y):
-        fit_params = self.fit_params.copy()
-        if "categorical_feature" in self.fit_params:
-            fit_params["categorical_feature"] = [cat for cat in fit_params["categorical_feature"] if cat in X.columns]
+    def _get_cv_score(self, feature_to_remove):
+        X, fit_params = self.dataset.getX(feature_to_remove=feature_to_remove, fit_params=self.fit_params)
+        y = self.dataset.y
 
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             cv_results = cross_validate(self.model, X, y, cv=self.cv, scoring=self.scoring, fit_params=fit_params)
         return cv_results['test_score']
 
-    def _get_cv_score_parallel(self, feature, feature_list, result_queue):
-        test_score = self._get_cv_score(self.df[feature_list], self.df[self.target])
+    def _get_cv_score_parallel(self, feature, result_queue):
+        test_score = self._get_cv_score(feature_to_remove=feature)
         result_queue.put((feature, test_score))
         return test_score
 
     def get_importance(self):
-        base_cv_score = self._get_cv_score(self.df[self.features], self.df[self.target])
+        base_cv_score = self._get_cv_score(feature_to_remove=None)
+        feature_list = self.dataset.features + list(self.dataset.feature_groups.keys())
 
         if self.n_jobs is not None and self.n_jobs > 1:
+
             pool = multiprocessing.Pool(self.n_jobs)
             manager = multiprocessing.Manager()
             result_queue = manager.Queue()
 
-            for f in self.features:
-                feature_list = [feature for feature in self.features if feature != f]
-                pool.apply_async(self._get_cv_score_parallel, (f, feature_list, result_queue))
+            for f in feature_list:
+                pool.apply_async(self._get_cv_score_parallel, (f, result_queue))
 
             pool.close()
             pool.join()
 
-            lofo_cv_scores = [result_queue.get() for _ in range(len(self.features))]
-            lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score
-                                                  for f, lofo_cv_score in lofo_cv_scores])
-            self.features = [score[0] for score in lofo_cv_scores]
+            lofo_cv_result = [result_queue.get() for _ in range(len(feature_list))]
+            lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score for _, lofo_cv_score in lofo_cv_result])
+            feature_list = [feature for feature, _ in lofo_cv_result]
         else:
             lofo_cv_scores = []
-            for f in tqdm_notebook(self.features):
-                feature_list = [feature for feature in self.features if feature != f]
-                lofo_cv_scores.append(self._get_cv_score(self.df[feature_list], self.df[self.target]))
+            for f in tqdm_notebook(feature_list):
+                lofo_cv_scores.append(self._get_cv_score(feature_to_remove=f))
 
             lofo_cv_scores_normalized = np.array([base_cv_score - lofo_cv_score for lofo_cv_score in lofo_cv_scores])
 
         importance_df = pd.DataFrame()
-        importance_df["feature"] = self.features
+        importance_df["feature"] = feature_list
         importance_df["importance_mean"] = lofo_cv_scores_normalized.mean(axis=1)
         importance_df["importance_std"] = lofo_cv_scores_normalized.std(axis=1)
 
