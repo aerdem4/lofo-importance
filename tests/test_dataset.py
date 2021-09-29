ca98c5f9694f718dec4a68210be1df70647df30b
Ahmet Erdem <ahmeterd4@gmail.com>
2021-09-29 09:25:05 +0300

    Group highly correlated features together (optional) (#38)
    Group highly correlated features together (optional)

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,7 +5,7 @@ from lofo import Dataset
 
 def test_dataset():
     df = generate_test_data(1000, text=True)
-    features = ["A", "B", "C", "D"]
+    features = ["A", "B", "C", "D", "D2"]
 
     # Exception: feature group row count is not equal to the features' row count
     feature_groups = {"interactions": df[["A", "B"]].values[:10]*df[["C", "D"]].values[:10]}
@@ -21,3 +21,8 @@ def test_dataset():
     feature_groups = {"E": df[["A", "B"]]}
     with pytest.raises(Exception):
         assert Dataset(df=df, target="binary_target", features=features, feature_groups=feature_groups)
+
+    d = Dataset(df=df, target="binary_target", features=features, feature_groups={"E": df[["A", "B"]].values},
+                auto_group_threshold=0.5)
+    assert "D" not in d.features and "D2" not in d.features
+    assert "D & D2" in d.feature_groups.keys() and "E" in d.feature_groups.keys()
